b859b9b457b989244cc1bfeeaf69427a39715e89
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2023-01-28 13:38:23 +0530

    config,controllers,models

---
+++ Controllers/user.controller.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../Models/user.model");
+
+const userRegister = async (req, res) => {
+  const { name, email, password } = req.body;
+  const present = await User.findOne({ email });
+  if (present) {
+    res.send("User already exists");
+  } else {
+    bcrypt.hash(password, 4, async function (err, hash) {
+      if (err) {
+        res.send("Something went wrong. Please try again!");
+      } else {
+        const user = new User({ name, email, password: hash });
+        await user.save();
+        res.send("User registerd successfully");
+      }
+    });
+  }
+};
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (user === null) {
+    res.status(401).send("Wrong credentials");
+  } else {
+    const hash = user.password;
+    const name = user.name;
+    const userId = user._id;
+    bcrypt.compare(password, hash, function (err, result) {
+      if (result) {
+        const token = jwt.sign({ id: userId }, process.env.SECRET_KEY);
+        res.send({ msg: "Login Successfull!!", name, token: token });
+      } else {
+        res.status(401).send("Wrong credentials");
+      }
+    });
+  }
+};
+
+module.exports = { userRegister, userLogin };
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connection = require("./config/db");
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const cors = require("cors");
+
+const { userRegister, userLogin } = require("./Controllers/user.controller");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.post("/register", userRegister);
+app.post("/login", userLogin);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to db");
+  } catch (error) {
+    console.log("Error in connecting", error.message);
+  }
+  console.log(`Listening on port ${PORT}`);
+});
